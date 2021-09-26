b999d605107e4a2f1f11ae30604f818ab1f1ec1f
Mhedii <mehedi1234.mh@gmail.com>
2021-09-26 21:25:46 +0600

    second commit

---
+++ src/components/Chef/Chef.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Chef.css';
+
+const Chef = () => {
+    return (
+        <div className="">
+            <h1>df</h1>
+        </div>
+    );
+};
+export default Chef;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Chef <span>Bazar</span></h1>
+            <h3>Total Budget:</h3>
+        </div>
+    );
+};
+export default Header;
